21a0bb69f34456e297ea65ed70978685e6a0654a
Sam Trouy <trouys16@gmail.com>
2021-03-14 18:26:13 -0400

    appspec

--- server.go
+++ server.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
-	log.Println("listening on 8080...")
-	log.Fatal(http.ListenAndServe("0.0.0.0:80", http.FileServer(http.Dir("site/"))))
+	sm := http.NewServeMux()
+	sm.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Write([]byte("Hello!\r\n"))
+	})
+	log.Println("Listening on port 8080.....")
+	l, err := net.Listen("tcp4", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Fatal(http.Serve(l, sm))
 }
