c6cc083ba4b852befedee437fd55dc71afb833a3
Sam Trouy <trouys16@gmail.com>
2021-03-14 16:59:44 -0400

    appspec

--- server.go
+++ server.go
@@ -7,5 +7,5 @@ import (
 
 func main() {
 	log.Println("listening on 8080...")
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("site/"))))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", http.FileServer(http.Dir("site/"))))
 }
