20ed164168a78b0be8564a8ebc7bf9b581d3cbb3
Sam Trouy <trouys16@gmail.com>
2021-03-13 20:47:47 -0500

    fixed .go server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/smltr/mysite
+
+go 1.13
--- site.go
+++ site.go
@@ -6,5 +6,6 @@ import (
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
+	log.Println("listening on 8080...")
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("site/"))))
 }
