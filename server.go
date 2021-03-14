d59214554283963471bc53030ae56a40b5a5a0a1
Sam Trouy <trouys16@gmail.com>
2021-03-14 18:52:07 -0400

    I think I finally got it this time

--- server.go
+++ server.go
@@ -7,15 +7,11 @@ import (
 )
 
 func main() {
-	sm := http.NewServeMux()
-	sm.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte("Hello!\r\n"))
-	})
+	http.Handle("/",  http.FileServer(http.Dir("site")))
 	log.Println("Listening on port 8080.....")
 	l, err := net.Listen("tcp4", ":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Fatal(http.Serve(l, sm))
+	log.Fatal(http.Serve(l, nil))
 }
