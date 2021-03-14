a93f0209c4c274d96c3d27e9acf7ba3a75162896
Sam Trouy <trouys16@gmail.com>
2021-03-14 16:23:26 -0400

    appspec

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 tmux new -d -s mysite-session
-tmux send -t mysite-session cd /home/ubuntu/mysite ENTER
+tmux send -t mysite-session cd SPACE /home/ubuntu/mysite ENTER
 tmux send -t mysite-session sudo SPACE ./server ENTER
