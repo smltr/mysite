34f62cdfd846c43273fea1414671028c639f37f6
Sam Trouy <trouys16@gmail.com>
2021-03-17 15:18:11 -0400

    testing

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Launching server..."
 tmux new -d -s mysite-session
 tmux send -t mysite-session cd SPACE /home/ubuntu/mysite ENTER
 tmux send -t mysite-session sudo SPACE ./server ENTER
+echo "Server launched."
