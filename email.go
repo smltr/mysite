06b99671a75e4001da4d353c543f41123b6a1bbd
Sam Trouy <trouys16@gmail.com>
2021-03-18 13:15:20 -0400

    fix typo

--- email.go
+++ email.go
@@ -35,7 +35,7 @@ func main() {
 // If you need more information about configurations or implementing the sample code, visit the AWS docs:   
 // https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/setting-up.html
 
-func getSecret() {
+func getSecret() string {
 	secretName := "smtppassword"
 	region := "us-east-2"
 
@@ -96,4 +96,6 @@ func getSecret() {
 		}
 		decodedBinarySecret = string(decodedBinarySecretBytes[:len])
 	}
+
+	return decodedBinarySecret
 }
